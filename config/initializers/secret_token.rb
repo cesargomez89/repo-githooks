65a3de7169dd63746022bad015c5cab6fcc9a273
Cesar Gomez <cesargomez89@gmail.com>
2013-10-31 17:06:51 -0600

    started project with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RepoGithooks::Application.config.secret_key_base = 'd8dc43b11c2274ace7e0272dffcb388c74ea2b5a6793940ac9632e0d170d5cd5200b0ecd978734a9fe2d3b574d55e22fb35ae8ee7c81002aa7469cb5daf652ee'
