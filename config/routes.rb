37ab02478ef179f99ca5caf5dbc1af6cf0527e93
Cesar Gomez <cesargomez89@gmail.com>
2013-11-04 12:53:10 -0600

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ RepoGithooks::Application.routes.draw do
 
   resources :welcome, only: [:index]
 
-  resources :hooks, except: :all do
+  resources :hooks, only: :index do
     collection do
       post :github
     end
