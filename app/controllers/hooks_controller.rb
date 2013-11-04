36bb3d0b21c6ce2b99ff501f292ae9a954de66ad
Cesar Gomez <cesargomez89@gmail.com>
2013-11-04 12:05:56 -0600

    some logic for hooks

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,5 +1,11 @@
 class HooksController < ApplicationController
   def github
-    binding.pry
+    User.create(
+      uid:      Random.new.rand(1..1000000).to_s,
+      provider: 'github',
+      username: Faker::Internet.email,
+      email:    Faker::Internet.user_name,
+      password: '12345678'
+    )
   end
 end
