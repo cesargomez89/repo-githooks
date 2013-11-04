d3bc35883cb63dbe040009c1455b21ad23de654a
Cesar Gomez <cesargomez89@gmail.com>
2013-11-04 12:13:15 -0600

    github controller

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,11 +1,14 @@
 class HooksController < ApplicationController
   def github
-    User.create(
+    user = User.create(
       uid:      Random.new.rand(1..1000000).to_s,
       provider: 'github',
       username: Faker::Internet.email,
       email:    Faker::Internet.user_name,
       password: '12345678'
     )
+    if user.presisted?
+      redirect_to :root
+    end
   end
 end
