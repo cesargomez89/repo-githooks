bfac9fe70eea69211a8e74c74690cd4bff62e8d3
Cesar Gomez <cesargomez89@gmail.com>
2013-11-04 14:05:01 -0600

    fixed controller github

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -2,13 +2,20 @@ require 'faker'
 class HooksController < ApplicationController
 
   def github
-   User.create(
+   user = User.build(
       uid:      Random.new.rand(1..1000000).to_s,
       provider: 'github',
       username: Faker::Internet.email,
       email:    Faker::Internet.user_name,
       password: '12345678'
     )
+    respond_to do |format|
+      if user.save
+        format.json { render json: { message: 'successfully added user' }, status: :ok }
+      else
+        format.json { render json: { message: 'You cannot create the user' }, status: :unprocessable_entity }
+      end
+    end
   end
 
   def hook_params
